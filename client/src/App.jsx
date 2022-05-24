fdfff8cae132ea37a473b3e5f02ecfdec3cb3cf3
Dhithen <calvaroj@uninorte.edu.co>
2022-05-24 07:52:34 -0500

    Last setup before deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,57 @@
-import React from 'react'
+import React,{useState} from 'react'
 //dependencias para la Api del chat
 import {StreamChat} from 'stream-chat';
 import {Chat} from 'stream-chat-react';
 import Cookies from 'universal-cookie';
+import 'stream-chat-react/dist/css/index.css'
 
 //Importar los componentes
-import { ChannelContainer, ChannelListContainer } from './components';
+import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 import './App.css'
 
-const apiKey= 'ju9n9wpw7ze8';
+const cookies = new Cookies();
 
+const apiKey= 'ju9n9wpw7ze8';
 const client=StreamChat.getInstance(apiKey);
+const authToken =cookies.get("token");
+
+if(authToken){
+  client.connectUser({
+    id: cookies.get('userId'),
+    token: cookies.get('token'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
+    },authToken);
+}
 
 //Creation off the app
 const App = () => {
+  const [createType, setCreateType]=useState('');
+  const [isCreating, setIsCreating]=useState(false);
+  const [isEditing, setIsEditing]=useState(false);
+
+
+  if(!authToken)return <Auth/>
+  
   return (
     <div className='app__wrapper'>
         <Chat client={client} theme='team light'>
-            <ChannelListContainer/>
-            <ChannelContainer/>
+            <ChannelListContainer
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setCreateType={setCreateType}
+              setIsEditing={setIsEditing}
+            />
+            <ChannelContainer
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              isEditing={isEditing}
+              setIsEditing={setIsEditing}
+              createType={createType}/>
         </Chat>
     </div>
   )
